6277c036c03ddf290c6634990ac57a59865a0306
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2024-07-08 20:44:58 -0400

    add: comments and fnsh first MVP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 // https://eth-sepolia.g.alchemy.com/v2/8PiV1eJiecx-k9WgE-aa-yZQ5RsdUYrY
 
 function App() {
+  // the provider is the connection to the blockchain
   const RPCProvider = "https://eth-sepolia.g.alchemy.com/v2/xfnApveI4Et5xdxgicivAdAbWsKTt3LY"
+  // the contract address of the smart contract
   const contractAddress = "0xD8466f8C9846955Dcc7415b1F06b766E14c663d7";
+  // the contract ABI (Application Binary Interface) is the definition of the smart contract
   const contractABI = [{ "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": false, "internalType": "string", "name": "_message", "type": "string" }], "name": "NewEventMessage", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": false, "internalType": "string", "name": "_name", "type": "string" }], "name": "NewEventName", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "_number", "type": "uint256" }], "name": "NewEventNumber", "type": "event" }, { "inputs": [{ "internalType": "uint256", "name": "_value", "type": "uint256" }], "name": "addToNumber", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_message", "type": "string" }], "name": "createEventMessage", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_name", "type": "string" }], "name": "createEventName", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getNumber", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }];
   const [provider, setProvider] = useState(null);
   const [events, setEvents] = useState([]);
 
-  // Conexión a la blockchain
+  // blockchain connection
   useEffect(() => {
     const connect = async () => {
       try {
@@ -24,7 +26,7 @@ function App() {
     connect();
   }, []);
 
-  // Obtener eventos del contrato
+  // get events from the smart contract
   useEffect(() => {
     if (provider && contractAddress && contractABI) {
       const contract = new ethers.Contract(contractAddress, contractABI, provider);
@@ -44,19 +46,25 @@ function App() {
       };
     }
   }, [provider, contractAddress, contractABI]);
+  // a simple UI to show the events
   return (
     <div className="App">
-      {/* Código para mostrar la interfaz de usuario */}
       <h2>Eventos del Contrato:</h2>
       <ul>
         {events.map((evento, index) => (
           <li key={index}>
-            {evento.nombre}: {evento.argumentos.map(arg => `${arg.nombre}: ${arg.valor}`).join(', ')}
+            <h3>{evento.nombre}:</h3>
+            <ul>
+              {evento.argumentos.map(arg => (
+                <li key={arg.nombre}>{arg.nombre}: {arg.valor}</li>
+              ))}
+            </ul>
           </li>
         ))}
       </ul>
     </div>
   );
+
 }
 
 export default App;
