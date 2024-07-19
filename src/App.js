8712f4a79f77e0b282daeee28578e0bc59974fe1
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2024-07-18 22:11:05 -0400

    add: interval for useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
+import Header from './components/Header';
 // https://eth-sepolia.g.alchemy.com/v2/8PiV1eJiecx-k9WgE-aa-yZQ5RsdUYrY
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
   const contractABI = [{ "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": false, "internalType": "string", "name": "_message", "type": "string" }], "name": "NewEventMessage", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": false, "internalType": "string", "name": "_name", "type": "string" }], "name": "NewEventName", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "_from", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "_number", "type": "uint256" }], "name": "NewEventNumber", "type": "event" }, { "inputs": [{ "internalType": "uint256", "name": "_value", "type": "uint256" }], "name": "addToNumber", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_message", "type": "string" }], "name": "createEventMessage", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_name", "type": "string" }], "name": "createEventName", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getNumber", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }];
   const [provider, setProvider] = useState(null);
   const [events, setEvents] = useState([]);
+  const [eventHash, setEventHash] = useState([]);
 
   // blockchain connection
   useEffect(() => {
@@ -31,15 +33,28 @@ function App() {
     if (provider && contractAddress && contractABI) {
       const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
-      contract.on("*", (event) => {
-        console.log("Evento: ", event.fragment.name);
-        let eventData = contract.interface.getEvent(event.fragment.name);
-        let eventInfo = { nombre: event.fragment.name, argumentos: [] };
-        for (let i = 0; i < eventData.inputs.length; i++) {
-          eventInfo.argumentos.push({ nombre: eventData.inputs[i].name, valor: event.args[i] });
-        }
-        setEvents(eventosAnteriores => [...eventosAnteriores, eventInfo]);
-      });
+      const interval = setInterval(() => {
+        contract.on("*", (event) => {
+          console.log("Evento: ", event.fragment.name);
+          let eventData = contract.interface.getEvent(event.fragment.name);
+          let eventH = event.log.transactionHash;
+          if (eventHash.includes(eventH)) {
+            return;
+          }
+          setEventHash(eventHash.push(eventH));
+          let eventInfo = { nombre: event.fragment.name, argumentos: [] };
+          for (let i = 0; i < eventData.inputs.length; i++) {
+            eventInfo.argumentos.push({ nombre: eventData.inputs[i].name, valor: event.args[i] });
+          }
+          setEvents(eventosAnteriores => {
+            const nuevosEventos = [...eventosAnteriores, eventInfo];
+            console.log(nuevosEventos); // Verifica que los eventos se estén actualizando correctamente
+            return nuevosEventos;
+          });
+        });
+
+      }, 2000);
+      return () => clearInterval(interval);
 
       return () => {
         contract.removeAllListeners();
@@ -48,21 +63,29 @@ function App() {
   }, [provider, contractAddress, contractABI]);
   // a simple UI to show the events
   return (
-    <div className="App">
-      <h2>Eventos del Contrato:</h2>
-      <ul>
-        {events.map((evento, index) => (
-          <li key={index}>
-            <h3>{evento.nombre}:</h3>
-            <ul>
-              {evento.argumentos.map(arg => (
-                <li key={arg.nombre}>{arg.nombre}: {arg.valor}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+
+      {/* informacion del contrato actual al que se está conectado, nombre y direccion: */}
+      <h4>Contract Address:</h4>
+      <p>{contractAddress}</p>
+
+      <div className="App">
+        <h3>Events:</h3>
+        <ul>
+          {events.map((evento, index) => (
+            <li key={index}>
+              <h4>{evento.nombre}:</h4>
+              <ul>
+                {evento.argumentos.map(arg => (
+                  <li key={arg.nombre}>{arg.nombre}: {String(arg.valor)}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+function Header() {
+    return (
+        <div className="alert alert-primary" role="alert">
+            <h1 className="blockmessage">Blockchain Event Tracker</h1>
+        </div>
+    );
+}
+export default Header;
